8a1419bc5aa98a8154b117f61ff61c3839d86f2c
taahahussainkhan <taaha128@gmail.com>
2023-04-14 07:01:01 +0500

    Menu added in Queue Program

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,86 +1,111 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Queue {
-    int* arr;
+class Queue
+{
+    int *arr;
     int front;
     int rear;
-    int size;  //Size for dynamic array
+    int size; // Size for dynamic array
 public:
-    //Default constructor
+    // Default constructor
     Queue()
     {
-        rear=-1;
-        front=-1;
-        arr= new int[10];
+        rear = -1;
+        front = -1;
+        arr = new int[10];
     }
-    //Parametrized Constructor
+    // Parametrized Constructor
     Queue(int size)
     {
-        this->size=size;
-        front=-1;
-        rear=-1;
+        this->size = size;
+        front = -1;
+        rear = -1;
         arr = new int[size];
     }
-    //Function to check if the Queue is full or not.
+    // Function to check if the Queue is full or not.
     bool isFull()
     {
-        return rear==size;
+        return rear == size;
     }
     bool isEmpty()
     {
-        return rear == -1 && front == -1 ;
+        return rear == -1 && front == -1;
     }
-    //enQueue function to perform enqueue operation on queue.
+    // enQueue function to perform enqueue operation on queue.
     void enQueue(int value)
     {
-        if(isFull())
-            cout<<"Queue Overflow!"<<endl;
-        else if(isEmpty())
-            rear=front=0;
-        else 
+        if (isFull())
+            cout << "Queue Overflow!" << endl;
+        else if (isEmpty())
+            rear = front = 0;
+        else
             rear++;
-        arr[rear]=value;
+        arr[rear] = value;
     }
     int deQueue()
     {
         int dq;
-        if(isEmpty())
-            cout<<"Queue Underflow!"<<endl;
-        
-        else if(rear==front)//Only first case when rear and front will be incremented, this step is applicable only first time.
+        if (isEmpty())
+            cout << "Queue Underflow!" << endl;
+
+        else if (rear == front) // Only first case when rear and front will be incremented, this step is applicable only first time.
         {
-            dq= arr[rear];
-            rear=-1;
-            front=-1;
+            dq = arr[front]; // As front and rear are both equal, so here in the [ ], we can put either rear or front.
+            rear = -1;
+            front = -1;
             return dq;
         }
         else
         {
-            dq=arr[front];
-            arr[front]=0;
-           front++;
+            dq = arr[front];
+            arr[front] = 0;
+            front++;
             return dq;
         }
-       
     }
     void display()
     {
-        for(int i=0;i<=rear;i++)
-            cout<<arr[i]<<" ";
+        for (int i = 0; i <= rear; i++)
+            cout << arr[i] << " ";
     }
-
 };
+void menu()
+{
+    int choice,en,de;
+    bool flag=true;
+    Queue q(10);
+    char oper;
+    do{       
+    cout<<"Enqueue."<<endl;
+    cout<<"Dequeue."<<endl;
+    cout<<"Display."<<endl;
+    cin>>choice;
+    switch (choice){
+        case 1:
+        cout<<"Enter value to enqueue: ";
+        cin>>en;
+        q.enQueue(en);
+        break;
+        case 2:
+        q.deQueue();
+        break;
+        case 3:
+        q.display();
+        break;
+        default:
+        cout<<"Wrong Input!"<<endl;
+    }
+    cout<<"\nDo you want to perform more operations y/n ? ";
+    cin>>oper;
+    if ( oper == 'y' || oper == 'Y' )
+    flag=true;
+    else
+    flag=false;
+    }while(flag);
+}
 int main()
 {
-
-    Queue q(10);
-    q.enQueue(23);
-    q.enQueue(16);
-    q.enQueue(97);
-    q.deQueue();
-    q.deQueue();
-    q.deQueue();
-    q.display();
+    menu();
     system("pause");
     return 0;
 }
