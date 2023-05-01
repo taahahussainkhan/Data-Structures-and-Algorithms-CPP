cd48b25066e6c6696f3a7914a4a5cacd5a9c4d80
taahahussainkhan <taaha128@gmail.com>
2023-05-01 17:01:05 +0500

    Infix to postfix fixed

--- Stack/Applications/infixTopostfix.cpp
+++ Stack/Applications/infixTopostfix.cpp
@@ -1,129 +1,104 @@
-#include <iostream>
-#include <string>
-#define SIZE 10
+#include<iostream>
+#include<stack>
 using namespace std;
-int precedence(char o);
-class Stack
-{
-    int *arr;
-    int size;
-    int top = -1;
 
-public:
-    Stack()
-    {
-        arr = new int[SIZE];
-    }
-    Stack(int size)
-    {
-        this->size = size;
-        arr = new int[size];
-    }
-    bool isEmpty()
-    {
-        return top == -1;
-    }
-    bool isFull()
-    {
-        return top == size;
-    }
-    void push(int value)
-    {
-        if (!isFull())
-        {
-            top++;
-            arr[top] = value;
-        }
-        else
-            cout << "Stack Overflow!" << endl;
-    }
-    int pop()
-    {
-        if (!isEmpty())
-        {
-            int p = arr[top];
-            top--;
-            return p;
-        }
-        else
-            cout << "Stack Underflow!" << endl;
-    }
-    int topv()
-    {
-        return arr[top];
-    }
-    void display()
-    {
-        cout << "Current Stack: " << endl;
-        for (int i = 0; i <= top; i++)
-        {
-            cout << arr[i] << " ";
-        }
-    }
-};
-int main()
+bool isOperator(char c)
 {
-    string infix, postfix[30];
-    cout << "Enter an infix expression: ";
-    cin >> infix;
-    Stack s(30);
-    int size1 = infix.size();
-    for (int i = 0; i < size1; i++)
-    {
-        if (infix[i] == '(')
-        {
-            s.push(infix[i]);
-        }
-        else if (infix[i] >= 'a' && infix[i] <= 'z')
-        {
-            cout << infix[i];
-        }
-        else if (infix[i] == ')')
-        {
-            if (infix[i] != '(')
-                s.pop();
-        }
-        else if ((infix[i] == '+') || (infix[i] == '-') || (infix[i] == '*') || (infix[i] == '/'))
-        {
-            if (s.isEmpty())
-            {
-                s.push(infix[i]);
-            }
-            else if(precedence(infix[i]) > precedence(s.topv()))
-            {
-                s.push(infix[i]);
-            }
-            else if (precedence(infix[i]) == precedence(s.topv()) || infix[i] == '^')
-            {
-                s.push(infix[i]);
-            }
-           else
+	if(c=='+'||c=='-'||c=='*'||c=='/'||c=='^')
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int precedence(char c) 
+{ 
+    if(c == '^') 
+    return 3; 
+    else if(c == '*' || c == '/') 
+    return 2; 
+    else if(c == '+' || c == '-') 
+    return 1; 
+    else
+    return -1; 
+} 
+
+string InfixToPostfix(stack<char> s, string infix)
+{
+	string postfix;
+	for(int i=0;i<infix.length();i++)
+	{
+		if((infix[i] >= 'a' && infix[i] <= 'z')
+		||(infix[i] >= 'A' && infix[i] <= 'Z'))
+		{
+			postfix+=infix[i];
+		}
+		else if(infix[i] == '(')
+		{
+			s.push(infix[i]);
+		}
+		else if(infix[i] == ')')
+		{
+			while((s.top()!='(') && (!s.empty()))
+			{
+				char temp=s.top();
+				postfix+=temp;
+				s.pop();
+			}
+			if(s.top()=='(')
+			{
+				s.pop();
+			}
+		}
+		else if(isOperator(infix[i]))
+		{
+			if(s.empty())
+			{
+				s.push(infix[i]);
+			}
+			else
+			{
+				if(precedence(infix[i])>precedence(s.top()))
 				{
-					while((!s.isEmpty())&&( precedence(infix[i])<=precedence(s.topv())))
+					s.push(infix[i]);
+				}	
+				else if((precedence(infix[i])==precedence(s.top()))&&(infix[i]=='^'))
+				{
+					s.push(infix[i]);
+				}
+				else
+				{
+					while((!s.empty())&&( precedence(infix[i])<=precedence(s.top())))
 					{
-						postfix[i] += s.topv();
+						postfix+=s.top();
 						s.pop();
 					}
 					s.push(infix[i]);
 				}
-        }
-    }
-     while(!s.isEmpty())
-    {
-        postfix[size1++] = s.topv();
-        s.pop();
-    }
+			}
+		}
+	}
+	while(!s.empty())
+	{
+		postfix+=s.top();
+		s.pop();
+	}
+	
+	return postfix;
+}
 
-    for(int i = 0; i < size1; i++)
-    {
-        cout<<postfix[i];
-    }
-    cout<<endl;
-    return 0;
-}
-int precedence(char o)
-{
-    if (o == '+' || o == '-') return 2;
-    else if (o == '*' || o == '/') return 1;
-    else    return -1;
-}
+int main() 
+{  
+
+  	string infix_exp, postfix_exp;
+  	cout<<"Enter a Infix Expression :"<<endl;
+  	cin>>infix_exp;
+  	stack <char> stack;
+	cout<<"INFIX EXPRESSION: "<<infix_exp<<endl;
+  	postfix_exp = InfixToPostfix(stack, infix_exp);
+  	cout<<endl<<"POSTFIX EXPRESSION: "<<postfix_exp;
+	  
+	
