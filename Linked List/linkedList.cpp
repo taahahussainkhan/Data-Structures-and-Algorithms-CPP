2b370821efe8ec01652419a67d704f1e0e786f80
taahahussainkhan <taaha128@gmail.com>
2023-05-04 12:53:27 +0500

    Node

--- Linked List/linkedList.cpp
+++ Linked List/linkedList.cpp
@@ -1,9 +1,49 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
+class Node
+{
+public:
+    int key;
+    int data;
+    Node *next;
+    Node()
+    {
+        key = 0;
+        data = 0;
+        next = NULL;
+    }
+    Node(int key, int data)
+    {
+        this->key = key;
+        this->data = data;
+    }
+};
+class SinglyLinkedList
+{
+public:
+    Node *head;
+    SinglyLinkedList()
+    {
+        head = NULL ;
+    }
+    SinglyLinkedList(Node *n)
+    {
+        this->head = n ;
+    }
+    Node nodeExists(int k)
+    {
+        Node* temp = NULL;
+        Node* ptr = head;
+        while (ptr != NULL)
+        {
+             if (ptr->key == k)
+             {
+                temp = ptr ; 
+             }
+        }
+    }
 
 };
 int main()
 {
-    
 }
