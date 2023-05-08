1dbd265438ebf1ad4930642788d82fd1de132486
taahahussainkhan <taaha128@gmail.com>
2023-05-09 02:08:05 +0500

    deletion and menu

--- Linked List/linkedList.cpp
+++ Linked List/linkedList.cpp
@@ -55,13 +55,41 @@ bool search(node *head, int key)
     }
     return false;
 }
+void deleteAtHead(node *&head)
+{
+    node *toDelete = head;
+    head = head->next;
+    delete toDelete;
+}
+void deletion(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        cout << "List is Empty! " << endl;
+        return;
+    }
+    if (head->next == NULL)
+    {
+        deleteAtHead(head);
+        return;
+    }
+    node *temp = head;
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+    node *toDelete = temp->next;
+    temp->next = temp->next->next;
+    delete toDelete;
+}
+
 int main()
 {
     node *head = NULL;
     int no_of_nodes, value;
     cout << "Number of Nodes: ";
     cin >> no_of_nodes;
-    for (int i = 0; i <= no_of_nodes; i++)
+    for (int i = 0; i < no_of_nodes; i++)
     {
         char insert;
         cout << "Where do you want ot insert value? ";
@@ -81,11 +109,31 @@ int main()
         }
     }
     display(head);
-    int key;
-    cout << "Value to search: ";
-    cin >> key;
-    if( search(head, key))
-        cout << key << " Found" << endl;
-    else 
-    cout << key <<" Not Found " << endl ;
+    int key, toDel;
+    char oper;
+    do
+    {
+        cout << "Operations" << endl;
+        cout << "Search S/s" << endl;
+        cout << "Deletion D/d" << endl;
+        cin >> oper;
+        if (oper == 'S' || oper == 's')
+        {
+            cout << "Value to search: ";
+            cin >> key;
+            if (search(head, key))
+                cout << key << " Found" << endl;
+            else
+                cout << key << " Not Found " << endl;
+        }
+        else if (oper == 'D' || oper == 'd')
+        {
+            cout << "Value to delete: ";
+            cin >> toDel;
+            deletion(head, toDel);
+            display(head);
+        }
+        else
+            exit(0);
+    } while (1);
 }
