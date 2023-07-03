6ba044f979bc69902efff02f739e15939f07ca7b
taahahussainkhan <taaha128@gmail.com>
2023-07-03 23:52:16 +0500

    Factorial recursion

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
 int factorial(int n){
+    //Base Case
     if(n==0)
         return 1;
-    return n*factorial(n-1);
+    //Recursive Case
+    int fact=n*factorial(n-1);
+    return fact;
 }
 int main()
 {
-    cout<<factorial(3)<<endl;
+    int n;
+    cin >> n;
+    cout<<factorial(n)<<endl;
 }
 
 /*
