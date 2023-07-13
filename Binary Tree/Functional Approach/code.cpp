1bca73de10592ebc7dbe24a23e334668304333d1
taahahussainkhan <taaha128@gmail.com>
2023-07-14 00:56:15 +0500

    Tree and traversal

--- Binary Tree/Functional Approach/code.cpp
+++ Binary Tree/Functional Approach/code.cpp
@@ -1,41 +1,50 @@
 #include<iostream>
 using namespace std;
-class node{
-    public:
+class Node{
+public:
+    Node* left;
+    Node* right;
     int data;
-    node* left;
-    node* right;
-    node(int d){
-        data=d;
-        left=right=NULL;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
     }
 };
-// Preorder building of the tree
-node* buildTree()
-{   
-    int data;
-    cout << "Enter data: ";
-    cin >> data;
-    if(data==-1)
-    return NULL;
-    node* n = new node(data);
-    n->left = buildTree();
-    n->right = buildTree();
-    return n;
-}
-void Preorder(node* root)
+void preorder(Node* root)
 {
-    if(root ==NULL)
-    {
-        return ;
-    }
-    cout << root->data << " ";
-    Preorder(root->left);
-    Preorder(root->right);
+    if (root ==NULL)return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void inorder(Node* root)
+{
+    if (root ==NULL)return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void postorder(Node* root)
+{
+    if (root ==NULL)return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
 }
 int main()
 {
-    node* root = buildTree();
-    Preorder(root);
-    return 0;
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right  = new Node(3);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    cout << "Preorder Traversal: ";
+    preorder(root);
+    cout << "\nInorder Traversal: ";
+    inorder(root);
+    cout << "\nPostorder Traversal: ";
+    postorder(root);
 }
